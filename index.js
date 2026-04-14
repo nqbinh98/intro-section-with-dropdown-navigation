0f774be749f3473c09fcc9ded25cb5b63584b439
nqbinh <154815917+nqbinh98@users.noreply.github.com>
2026-04-14 10:03:25 +0700

    Update js and css file

--- index.js
+++ index.js
@@ -11,33 +11,59 @@ const companyCollapsed = document.querySelector('.company-collapsed');
 const companyExpanded = document.querySelector('.company-expanded');
 const companyContent = document.querySelector('.company-content');
 
+const selectElement = document.querySelector('.select');
+
+selectElement.addEventListener('click', function (e) {
+    
+    let state;
+    if(e.target.closest('.features-select')) {
+        state = featuresContent.matches('.is-close-content');
+        handleFeaturesSelect(state)
+    } else if (e.target.closest('.company-select')) {
+        state = companyContent.matches('.is-close-content');
+        handleCompanySelect(state)
+    }   
+
+})
+
 function handleMenu () {
-    menuModal.classList.toggle('is-open');
     menuModal.classList.toggle('is-close');
+    menuModal.classList.toggle('is-open');
 }
 
-function handleFeaturesSelect () {
-    featuresCollapsed.classList.toggle('hide');
-    featuresExpanded.classList.toggle('hide');
-    featuresContent.classList.toggle('is-open-content');
-    featuresContent.classList.toggle('is-close-content');
+function handleFeaturesSelect (isClose) {
+    if (isClose === true) {
+        featuresExpanded.classList.remove('hide');
+        featuresCollapsed.classList.add('hide');
+        featuresContent.classList.remove('is-close-content');
+        featuresContent.classList.add('is-open-content');
+    } else {
+        featuresExpanded.classList.add('hide');
+        featuresCollapsed.classList.remove('hide');
+        featuresContent.classList.add('is-close-content');
+        featuresContent.classList.remove('is-open-content');
+    }
 }
 
-function handleCompanySelect () {
-    companyCollapsed.classList.toggle('hide');
-    companyExpanded.classList.toggle('hide');
-    companyContent.classList.toggle('is-open-content');
-    companyContent.classList.toggle('is-close-content');
+function handleCompanySelect (isClose) {
+    if (isClose === true) {
+        companyExpanded.classList.remove('hide');
+        companyCollapsed.classList.add('hide');
+        companyContent.classList.remove('is-close-content');
+        companyContent.classList.add('is-open-content');
+    } else {
+        companyExpanded.classList.add('hide');
+        companyCollapsed.classList.remove('hide');
+        companyContent.classList.add('is-close-content');
+        companyContent.classList.remove('is-open-content');
+    }
 }
+
 
 menuBtn.addEventListener('click', handleMenu);
 menuCloseBtn.addEventListener('click', handleMenu);
 menuModal.addEventListener('click', handleMenu);
 menuDropdown.addEventListener('click', function (e) {
-    e.preventDefault()
-    e.stopPropagation()
+    e.stopPropagation();
 })
 
-featuresSelect.addEventListener('click', handleFeaturesSelect);
-
-companySelect.addEventListener('click', handleCompanySelect);
